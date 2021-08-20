ea03a94176ff1235079af96be97ffd7e426d9619
nikch0 <36309764+nikch0@users.noreply.github.com>
2021-08-20 18:56:07 +0300

    Added an option to toggle the hideplayers on spawn

--- src/me/niko/kingdom/listeners/VisibilityListener.java
+++ src/me/niko/kingdom/listeners/VisibilityListener.java
@@ -5,12 +5,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import me.niko.kingdom.Kingdom;
 import me.niko.kingdom.visibility.VisibilityManager;
 
 public class VisibilityListener implements Listener {
 	
 	@EventHandler
 	public void onMove(PlayerMoveEvent event) {
+		if(!Kingdom.getInstance().getConfig().getBoolean("settings.hide_players_spawn")) {
+			return;
+		}
+		
 		if(event.getFrom().getBlockX() == event.getTo().getBlockX()
 	            && event.getFrom().getBlockY() == event.getTo().getBlockY()
 	            && event.getFrom().getBlockZ() == event.getTo().getBlockZ()) return;
